d34433bb3431f4e49dc9609978cdb55886b63691
Ronny Gonzalez <yordemblo@gmail.com>
2025-10-05 15:33:12 +0200

    Agrega los recursos de la clase 1

---
+++ curso-js/10-2025/codigos/tema1/variables_constantes.js
@@ -0,0 +1,11 @@
+// Esto es un comentario de una sola línea
+let nombre = "Itgrarte";
+// Esto es una constante
+const pi = 3.14;
+
+
+// Mostrando un mensaje
+// console.log("Valor del nombre: " + nombre);
+
+// Mostrando información en el html
+document.getElementById("saludo").textContent = `Saludo desde la fundación: ${nombre}`;
