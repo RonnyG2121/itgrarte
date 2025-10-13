18ba6fc672db2cf48fa30bf5392ce969f94c5dbb
Ronny Gonzalez <yordemblo@gmail.com>
2025-10-13 10:48:39 +0200

    Sube los recursos de la clase 2

---
+++ curso-js/10-2025/codigos/tema2/tipos_datos.js
@@ -0,0 +1,18 @@
+// Declarando un arreglo
+const estudiantes = [
+    "Carlos",
+    "Maria",
+    "Juan",
+    "Marta",
+    false,
+    5,
+    {}];
+
+    // Declarando objetos
+    const diccionario = {
+        amor: 10,
+        paz: 15,
+        lista_sinonimos: []
+    };
+
+console.log(diccionario.paz);
