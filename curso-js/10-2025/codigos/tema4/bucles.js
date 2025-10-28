6e0c0c50c2519cf4e30367abd870960e1ab3faee
Ronny Gonzalez <yordemblo@gmail.com>
2025-10-28 17:05:16 +0100

    Agrega los recursos de la clase 4

---
+++ curso-js/10-2025/codigos/tema4/bucles.js
@@ -0,0 +1,60 @@
+// Bucles
+const ALUMNOS = [
+    "Juan",
+    "Carla",
+    "Pedro",
+    "María"
+];
+
+// sintaxis del bucle for
+
+/* for (let i = 0; i< ALUMNOS.length; i++) {
+    console.info(i);
+    console.info(ALUMNOS[i]);
+}
+ */
+
+
+
+// Añadir elementos al arreglo
+// ALUMNOS.push("Lucas");
+
+// console.log(ALUMNOS[4]);
+
+
+// Sintaxis de bucle while
+// Bucle que no se ejecuta
+let numero = 0;
+/* while (numero> 5) {
+    console.info(numero);
+} */
+
+// Bucle para hacer un conte
+
+/* while (numero <5) {
+    numero++;
+    console.log(numero);
+}
+ */
+
+// Usar la instrucción continue
+
+/* while (numero <10) {
+    numero++;
+    if (numero === 4) {
+        continue;
+    }
+
+    console.info(numero);
+} */
+
+    // break
+/* while (numero <10) {
+    numero++;
+    if (numero === 5) {
+        break;
+    }
+    console.info(numero);
+} */
+
+    
